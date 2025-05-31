413b921bf13e3f161b133aa573f164fb3ba52e11
Davi Monteiro <davi.monteiro7@hotmail.com>
2025-05-31 11:52:47 -0300

    initial setup_

---
+++ slitherio-backend/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "slitherio-backend"
---
+++ slitherio-backend/src/main/java/com/example/slitherio/backend/SlitherioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.slitherio.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlitherioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlitherioBackendApplication.class, args);
+	}
+
+}
